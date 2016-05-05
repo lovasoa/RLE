3a1c557cdad5720ecff371ccf9695e07955bdf6c
Ophir LOJKINE <pere.jobs@gmail.com>
2016-05-05 20:00:52 +0300

    Create an RLE type

--- rle.hs
+++ rle.hs
@@ -1,10 +1,12 @@
+type RLE a = [(Int, a)]
+
 -- Encode a list using RLE
-rle :: Eq a => [a] -> [(Int, a)]
+rle :: Eq a => [a] -> RLE a
 rle [] = []
 rle (c:s) = let (eq, diff) = span (==c) s in (1+length eq, c):rle diff
 
 -- Encode an RLE to a string
-rle_to_s :: [(Int, Char)] -> String
+rle_to_s :: RLE Char -> String
 rle_to_s = concatMap (\(n,c) -> show n ++ [c])
 
 main = do
