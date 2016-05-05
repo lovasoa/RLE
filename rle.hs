bb100c29806ae41efff7a47a257e0c9b246ff42f
Ophir LOJKINE <pere.jobs@gmail.com>
2016-05-05 19:58:51 +0300

    Add comments

--- rle.hs
+++ rle.hs
@@ -1,7 +1,9 @@
+-- Encode a list using RLE
 rle :: Eq a => [a] -> [(Int, a)]
 rle [] = []
 rle (c:s) = let (eq, diff) = span (==c) s in (1+length eq, c):rle diff
 
+-- Encode an RLE to a string
 rle_to_s :: [(Int, Char)] -> String
 rle_to_s = concatMap (\(n,c) -> show n ++ [c])
 
